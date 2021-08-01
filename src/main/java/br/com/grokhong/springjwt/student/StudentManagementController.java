32c02a19bcecebe257cf96980a27c8351ba1e3d1
Paulo Almeida <estpaulovictora@live.com>
2021-08-01 15:14:24 -0300

    preAuthorize()

--- src/main/java/br/com/grokhong/springjwt/student/StudentManagementController.java
+++ src/main/java/br/com/grokhong/springjwt/student/StudentManagementController.java
@@ -3,6 +3,7 @@ package br.com.grokhong.springjwt.student;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,24 +19,30 @@ public class StudentManagementController {
     private final List<Student> STUDENTS = Arrays.asList(new Student(1, "Paulo"), new Student(2, "Pedro"),
             new Student(3, "Jefferson"));
 
+    // hasRole('ROLE_') hasAnyRole('ROLE_') hasAuthority('permission') hasAnyAuthority('permission')
+
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_ADMINTRAINEE')")
     public List<Student> getAllStudents() {
         return STUDENTS;
     }
     
     @PostMapping
+    @PreAuthorize("hasAuthority('student:write')")
     public void registerNewStudent(@RequestBody Student student) {
         System.out.println("registerNewStudent");
         System.out.println(student);
     }
 
     @DeleteMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void deleteStudent(@PathVariable("studentId") Integer studentId) {
         System.out.println("deleteStudent");
         System.out.println(studentId);
     }
 
     @PutMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void updateStudent(@PathVariable("studentId") Integer studentId, @RequestBody Student student) {
         System.out.println("updateStudent");
         System.out.println(String.format("%s %s", studentId, student));

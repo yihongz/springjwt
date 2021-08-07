47e83706724dc08709f48ad018d7f0c079f60ae1
Paulo Almeida <estpaulovictora@live.com>
2021-08-07 13:58:19 -0300

    Logout

--- src/main/java/br/com/grokhong/springjwt/security/ApplicationSecurityConfig.java
+++ src/main/java/br/com/grokhong/springjwt/security/ApplicationSecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -42,7 +43,15 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         .and()
         .rememberMe() // defaults to 2 weeks
             .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
-            .key("somethingverysecured");
+            .key("somethingverysecured")
+        .and()
+        .logout()
+            .logoutUrl("/logout")
+            .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET")) // if enable csrf and want to use get instead of post (not a best practice)
+            .clearAuthentication(true)
+            .invalidateHttpSession(true)
+            .deleteCookies("JSESSIONID", "remember-me")
+            .logoutSuccessUrl("/login");
     }
 
     @Override
